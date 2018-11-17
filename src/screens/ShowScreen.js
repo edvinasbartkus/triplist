92d7f260efa2e52ff8dec5e44fdc7a6d9cad2a84
Edvinas Bartkus <edvinas@bartkus.lt>
2018-11-18 00:25:37 +0200

    Maps added to the project

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'
-import {View, SafeAreaView, Text} from 'react-native'
+import {View, SafeAreaView, Text, StyleSheet} from 'react-native'
+import MapView from 'react-native-maps'
 
 export default class ShowScreen extends Component {
   render () {
     return (
       <SafeAreaView>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
         <View>
           <Text>This is show screen {JSON.stringify(this.props)}</Text>
         </View>
@@ -12,3 +22,10 @@ export default class ShowScreen extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: 300
+  }
+})
