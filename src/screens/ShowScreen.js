16a93d0bcd183e0384d958abf87b0e97ba70f13c
Edvinas Bartkus <edvinas@bartkus.lt>
2018-11-18 10:12:05 +0200

    Rendering custom markers

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -6,6 +6,8 @@ import DraggableFlatList from 'react-native-draggable-flatlist'
 import {updateList, findById} from './../utils/db'
 import {KEY} from './../utils/maps'
 import MapViewDirections from 'react-native-maps-directions'
+import {SwipeRow} from 'react-native-swipe-list-view'
+import {getColor} from '../utils/consts'
 
 export default class ShowScreen extends Component {
   static options(passProps) {
@@ -50,34 +52,49 @@ export default class ShowScreen extends Component {
     }
   }
 
-  renderMarker (place) {
+  renderMarker (place, index) {
     return (
       <Marker 
         key={place.name}
         coordinate={place.location}
         title={place.name}
-      />
+      >
+        <View style={[styles.marker, {backgroundColor: getColor(0)}]}>
+          <Text style={styles.markerText}>{index + 1}</Text>
+        </View>
+      </Marker>
     )
   }
 
   renderItem = ({ item, index, move, moveEnd, isActive }) => {
     return (
-      <TouchableOpacity
-        style={{ 
-          height: 100, 
-          backgroundColor: isActive ? 'blue' : '#333',
-          alignItems: 'center', 
-          justifyContent: 'center' 
-        }}
-        onLongPress={move}
-        onPressOut={moveEnd}
-      >
-        <Text style={{ 
-          fontWeight: 'bold', 
-          color: 'white',
-          fontSize: 32,
-        }}>{item.name}</Text>
-      </TouchableOpacity>
+      <SwipeRow leftOpenValue={75} rightOpenValue={-75}>
+        <TouchableOpacity style={styles.complete}>
+          <Text style={styles.completeText}>Complete</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.itemBox}
+          onLongPress={move}
+          onPressOut={moveEnd}
+        >
+          <View style={styles.itemContainer}>
+            <View style={styles.itemIconContainer}>
+              <View style={[styles.itemIcon, {backgroundColor: getColor(0)}]}>
+                <Text style={styles.itemIconText}>
+                  {index + 1}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.textsContainer}>
+              <Text style={styles.itemText}>{item.name}</Text>
+              <View></View>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <View style={{width: 75}}>
+          <Text>Delete</Text>
+        </View>
+      </SwipeRow>
     )
   }
 
@@ -92,7 +109,7 @@ export default class ShowScreen extends Component {
           longitudeDelta: 0.0421,
         }}
       >
-        {this.state.list.items.map(it => this.renderMarker(it))}
+        {this.state.list.items.map((it, index) => this.renderMarker(it, index))}
         {this.renderDirections()}
       </MapView>
     )
@@ -110,7 +127,7 @@ export default class ShowScreen extends Component {
           apikey={KEY}
           mode='walking'
           strokeWidth={3}
-          strokeColor="hotpink"
+          strokeColor={getColor(0)}
         />
       )
 
@@ -130,8 +147,10 @@ export default class ShowScreen extends Component {
           keyExtractor={(item, index) => `draggable-item-${item.name}`}
           onMoveEnd={async ({ data }) => {
             const list = {...this.state.list, items: data}
-            await updateList(list, this.state.list._id)
             this.setState({list})
+
+            // Keep it last so it doesn't affect the performance
+            await updateList(list, this.state.list._id)
           }}
         />
       </View>
@@ -151,5 +170,68 @@ const styles = StyleSheet.create({
 
   item: {
     padding: 10
+  },
+
+  itemContainer: {
+    flexDirection: 'row'
+  },
+
+  itemIconContainer: {
+    height: 50,
+    width: 50,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  itemIcon: {
+    width: 25,
+    height: 25,
+    borderRadius: 25,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  itemIconText: {
+    color: 'white'
+  },
+
+  itemBox: {
+    backgroundColor: 'white',
+    height: 50
+  },
+
+  textsContainer: {
+    justifyContent: 'center'
+  },
+
+  itemText: { 
+    fontSize: 22,
+  },
+
+  complete: {
+    width: 75,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green'
+  },
+
+  completeText: {
+    color: 'white'
+  },
+
+  marker: {
+    width: 15,
+    height: 15,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  markerText: {
+    color: 'white',
+    fontSize: 10
   }
 })
