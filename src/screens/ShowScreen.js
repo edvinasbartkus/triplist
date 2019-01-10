c7ee053d3279e0f12f54578c0d20648cbf67c4df
Edvinas Bartkus <edvinas@bartkus.lt>
2019-01-10 23:02:06 +0200

    Outsource <Ttem /> to separate component

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,16 +1,13 @@
 import React, {Component} from 'react'
-import {View, ActionSheetIOS, TouchableOpacity, Text, StyleSheet, TouchableHighlight} from 'react-native'
+import {StyleSheet} from 'react-native'
 import {Navigation} from 'react-native-navigation'
-import DirectionsContainer from '../containers/DirectionsContainer'
-import {getColor} from '../utils/consts'
 import Map from '../components/Map'
-import Circle from '../components/Circle'
 import SortableListView from 'react-native-sortable-listview'
-import SwipeableRow from 'react-native/Libraries/Experimental/SwipeableRow/SwipeableRow'
 import {Subscribe} from 'unstated'
 import ListsContainer from '../containers/ListsContainer'
 import { ActionSheetCustom as ActionSheet } from 'react-native-custom-actionsheet'
 import ModeButtons from '../components/ModeButtons'
+import Item from '../components/Item'
 
 ACTION_SHEET_OPTIONS = [
   'Cancel',
@@ -168,154 +165,9 @@ export default class ShowScreen extends Component {
   }
 }
 
-class Item extends React.Component {
-  renderETA (directions, item) {
-    if (item.previous) {
-      const route = directions.get(item.mode, item.previous.location, item.location)
-      if (route) {
-        let {distance, duration} = route
-        distance = distance > 10 ? Math.round(distance) : Math.round(distance * 10) / 10
-        duration = Math.round(duration)
-        return (
-          <TouchableOpacity onPress={() => this.props.changeMode(item)}>
-            <Text style={styles.subline}>
-              {distance}km of {item.mode} in {duration} {duration > 1 ? 'minutes' : 'minute'}
-            </Text>
-          </TouchableOpacity>
-        )
-      } else {
-        return (
-          <TouchableOpacity onPress={() => this.props.changeMode(item)}>
-            <Text style={styles.subline}>{item.mode}</Text>
-          </TouchableOpacity>
-        )
-      }
-    } else {
-      return (
-        <Text style={styles.subline}>Starting point</Text>
-      )
-    }
-  }
-
-  render () {
-    const {item} = this.props
-
-    return (
-      <SwipeableRow
-        isOpen={false}
-        shouldBounceOnMount={false}
-        maxSwipeDistance={75}
-        preventSwipeRight={true}
-        slideoutView={
-          <View style={styles.rowBack}>
-            <View style={styles.complete} />
-            <TouchableOpacity onPress={() => this.props.onDelete(item)} style={styles.delete}>
-              <Text style={styles.deleteText}>Delete</Text>
-            </TouchableOpacity>
-          </View>
-        }
-      >
-        <TouchableHighlight underlayColor={'#F5F5F5'} {...this.props.sortHandlers}>
-          <View style={styles.itemContainer}>
-            <View style={styles.itemIconContainer}>
-              <Circle
-                onPress={() => this.props.onComplete(item)}
-                text={item.index}
-                color={getColor(0)}
-                completed={item.completed} />
-            </View>
-            <View style={styles.textsContainer}>
-              <Text style={styles.itemText}>{item.name}</Text>
-              <View style={styles.actionsContainer}>
-                <Subscribe to={[DirectionsContainer]}>{directions => this.renderETA(directions, item)}</Subscribe>
-              </View>
-            </View>
-          </View>
-        </TouchableHighlight>
-      </SwipeableRow>
-    )
-  }
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  item: {
-    padding: 10
-  },
-
   list: {
     flex: 1,
     marginBottom: 0
   },
-
-  itemContainer: {
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-
-  itemIconContainer: {
-    height: 50,
-    width: 50,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  itemBox: {
-    backgroundColor: 'white',
-    height: 50
-  },
-
-  textsContainer: {
-    justifyContent: 'center'
-  },
-
-  actionsContainer: {
-    flexDirection: 'row'
-  },
-
-  itemText: { 
-    fontSize: 22,
-  },
-
-  rowBack: {
-    alignItems: 'center',
-    backgroundColor: '#DDD',
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-
-  complete: {
-    width: 75,
-    height: 70,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  delete: {
-    width: 75,
-    height: 70,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red'
-  },
-
-  deleteText: {
-    color: 'white'
-  },
-
-  completeText: {
-    color: 'white'
-  },
-
-  subline: {
-    fontSize: 12,
-    color: '#666'
-  }
 })
